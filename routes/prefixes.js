ab091d4b5057304f7f4da16888930633ad9ad0fa
reptar25 <bailey.kevin89@gmail.com>
2021-03-31 15:19:55 -0700

    Reformat code

--- routes/prefixes.js
+++ routes/prefixes.js
@@ -3,32 +3,35 @@ const express = require('express');
 const router = express.Router();
 const client = require('../dbClient');
 
-router.route('/').get((req, res) => {
-    client
-        .query('SELECT * FROM prefixes')
-        .then(result => res.json(result.rows))
-        .catch(e => console.error(e));
-}).post((req, res) => {
-    const sql = 'INSERT INTO prefixes (guild_id, prefix) VALUES ($1, $2) ON CONFLICT (guild_id) DO UPDATE SET prefix = $2';
-    const values = [req.body.guild_id, req.body.prefix];
+router.route('/')
+    .get((req, res) => {
+        client
+            .query('SELECT * FROM prefixes')
+            .then(result => res.json(result.rows))
+            .catch(e => console.error(e));
+    })
+    .post((req, res) => {
+        const sql = 'INSERT INTO prefixes (guild_id, prefix) VALUES ($1, $2) ON CONFLICT (guild_id) DO UPDATE SET prefix = $2';
+        const values = [req.body.guild_id, req.body.prefix];
 
-    client
-        .query(sql, values)
-        .then(result => {
-            res.json(result);
-        })
-        .catch(e => console.error(e));
-}).delete((req, res) => {
-    const sql = 'DELETE FROM prefixes WHERE guild_id = $1';
-    const values = [req.body.guild_id];
+        client
+            .query(sql, values)
+            .then(result => {
+                res.json(result);
+            })
+            .catch(e => console.error(e));
+    })
+    .delete((req, res) => {
+        const sql = 'DELETE FROM prefixes WHERE guild_id = $1';
+        const values = [req.body.guild_id];
 
-    client
-        .query(sql, values)
-        .then(result => {
-            res.send(result);
-        })
-        .catch(e => console.error(e));
-});
+        client
+            .query(sql, values)
+            .then(result => {
+                res.send(result);
+            })
+            .catch(e => console.error(e));
+    });
 
 router.get('/:guildId', (req, res, next) => {
     client
